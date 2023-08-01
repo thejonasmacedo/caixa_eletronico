c13f3108cb3ecd336decf8f6e4dcadad102c4c7f
thejonasmacedo <jonas.b.m@outlook.com>
2023-08-01 20:45:28 -0300

    limpando coisas inúteis do código e concertando certos parâmetros

--- caixa_eletrônico.py
+++ caixa_eletrônico.py
@@ -13,7 +13,6 @@ menu = '''
 '''
 x = True
 deposito = []
-na_conta = 0
 saque = []
 extrato = {'deposito': [], 'saque': []}
 saldo_total = 0
@@ -28,7 +27,6 @@ while x:
 		
 		quant_deposito = float(input('Quanto deseja depositar?: R$ '))
 		deposito.append(quant_deposito)
-		na_conta += quant_deposito
 		extrato['deposito'].append(quant_deposito)
 
 		saldo_total += quant_deposito
@@ -38,18 +36,13 @@ while x:
 		
 		if quant_saque <= saldo_total:
 			saque.append(quant_saque)
-			na_conta += quant_saque
-			extrato['saque'].append(quant_deposito)
+			extrato['saque'].append(quant_saque)
 			saldo_total -= quant_saque
 		else:
 			print("ATENÇÃO! NÃO HÁ SALDO SUFICIENTE NA CONTA!")
 
 	
 	elif question == 'e':
-		
-		# tamanho_extrato_deposito = len(extrato['deposito'])
-		# tamanho_extrato_saque = len(extrato['saque'])
-		
 
 		if bsidict(extrato) == 0:
 			min_to_run = 1
@@ -57,18 +50,6 @@ while x:
 		else:
 			min_to_run = bsidict(extrato)
 
-		# for i in range(min_to_run):
-		# 	if bsidict(extrato) == 0:
-		# 		print ('depósito: ---------- | saque: ---------- ')
-			
-		# 	elif not extrato['deposito'][i]:
-		# 		print (f"depósito: ---------- | saque: {extrato['saque'][i-1]} ")
-			
-		# 	elif not extrato['saque'][i]:
-		# 		print(f"depósito: {extrato['saque'][i-1]} | saque: ---------- ")
-
-		# 	else:
-		# 		print(f"depósito: {extrato['saque'][i]} | saque: {extrato['saque'][i]} ")
 		for i in range(min_to_run):
 			deposito_info = extrato['deposito'][i] if i < len(extrato['deposito']) else '----------'
 			saque_info = extrato['saque'][i] if i < len(extrato['saque']) else '----------'
