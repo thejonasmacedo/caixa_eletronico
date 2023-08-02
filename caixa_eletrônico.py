9b2b09f3a4dc03d0102ba5cd0a8128da4ecc53fe
thejonasmacedo <jonas.b.m@outlook.com>
2023-08-02 20:48:35 -0300

    Commit feito antes da transformação para commits

--- caixa_eletrônico.py
+++ caixa_eletrônico.py
@@ -2,6 +2,66 @@
 # sys.path.insert(0, '/home/jx/Documents/hardware e software/git_projects/tools_py')
 from item_biggest_size import bsidict
 
+def deposito():
+	quant_deposito = float(input('Quanto deseja depositar?: R$ '))
+
+	if quant_deposito < 0:
+		print("Valor de depósito é inválido! Por favor, ao tentar novamente insira um valor positivo!\n")
+	else:
+		deposito.append(quant_deposito)
+		extrato['deposito'].append(quant_deposito)
+		
+		saldo_total += quant_deposito
+
+def saque():
+	quant_saque = float(input('Quanto deseja saque?: R$ '))
+		
+	if quant_saque <= saldo_total and LIMITE_DIARIO > 0:
+		if quant_saque > 0:
+			saque.append(quant_saque)
+			extrato['saque'].append(quant_saque)
+			saldo_total -= quant_saque
+			LIMITE_DIARIO -= 1
+		else:
+			print('\nValor de saque é inválido! Por favor, ao tentar novamente insira um valor positivo!')
+
+		if LIMITE_DIARIO <= 0:
+			print("\nATENÇÃO! LIMITE DIÁRIO DE SAQUES EXCEDIDO!")
+		
+		if quant_saque > saldo_total:
+			print("\nATENÇÃO! NÃO HÁ SALDO SUFICIENTE NA CONTA!")
+	
+
+def extrato():
+	if bsidict(extrato) == 0:
+		min_to_run = 1
+	
+	else:
+		min_to_run = bsidict(extrato)
+	
+	for i in range(min_to_run):
+		deposito_info = extrato['deposito'][i] if i < len(extrato['deposito']) else '/----------/'
+		saque_info = extrato['saque'][i] if i < len(extrato['saque']) else '/----------/'
+		print('\n--------------------------- EXTRATO -------------------------------')
+		print(f"depósito: {deposito_info} | saque: {saque_info}")
+		print(f'\nSaldo Total: R${saldo_total}')
+		print('---------------------------------------------------------------------')
+
+	
+
+def criar_conta_cliente():
+	...
+
+def criar_conta_corrente():
+	...
+
+def sair():
+	...
+
+
+
+
+
 menu = '''
 --------------------------------
 		[d]epósito
@@ -23,35 +83,35 @@ while x:
 	if question == 'm':
 		print(menu)
 	
-	elif question == 'd':
+	# elif question == 'd':
 		
-		quant_deposito = float(input('Quanto deseja depositar?: R$ '))
+	# 	quant_deposito = float(input('Quanto deseja depositar?: R$ '))
 		
-		if quant_deposito < 0:
-			print("Valor de depósito é inválido! Por favor, ao tentar novamente insira um valor positivo!\n")
-		else:
-			deposito.append(quant_deposito)
-			extrato['deposito'].append(quant_deposito)
+	# 	if quant_deposito < 0:
+	# 		print("Valor de depósito é inválido! Por favor, ao tentar novamente insira um valor positivo!\n")
+	# 	else:
+	# 		deposito.append(quant_deposito)
+	# 		extrato['deposito'].append(quant_deposito)
 
-			saldo_total += quant_deposito
+	# 		saldo_total += quant_deposito
 
-	elif question == 's':
-		quant_saque = float(input('Quanto deseja saque?: R$ '))
+	# elif question == 's':
+	# 	quant_saque = float(input('Quanto deseja saque?: R$ '))
 		
-		if quant_saque <= saldo_total and LIMITE_DIARIO > 0:
-			if quant_saque > 0:
-				saque.append(quant_saque)
-				extrato['saque'].append(quant_saque)
-				saldo_total -= quant_saque
-				LIMITE_DIARIO -= 1
-			else:
-				print('\nValor de saque é inválido! Por favor, ao tentar novamente insira um valor positivo!')
+	# 	if quant_saque <= saldo_total and LIMITE_DIARIO > 0:
+	# 		if quant_saque > 0:
+	# 			saque.append(quant_saque)
+	# 			extrato['saque'].append(quant_saque)
+	# 			saldo_total -= quant_saque
+	# 			LIMITE_DIARIO -= 1
+	# 		else:
+	# 			print('\nValor de saque é inválido! Por favor, ao tentar novamente insira um valor positivo!')
 		
-		if LIMITE_DIARIO <= 0:
-			print("\nATENÇÃO! LIMITE DIÁRIO DE SAQUES EXCEDIDO!")
+	# 	if LIMITE_DIARIO <= 0:
+	# 		print("\nATENÇÃO! LIMITE DIÁRIO DE SAQUES EXCEDIDO!")
 		
-		if quant_saque > saldo_total:
-			print("\nATENÇÃO! NÃO HÁ SALDO SUFICIENTE NA CONTA!")
+	# 	if quant_saque > saldo_total:
+	# 		print("\nATENÇÃO! NÃO HÁ SALDO SUFICIENTE NA CONTA!")
 
 	
 	elif question == 'e':
