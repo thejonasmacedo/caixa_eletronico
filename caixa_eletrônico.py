9eb42b42d07568d80dd4c35126e7a305654251f6
thejonasmacedo <jonas.b.m@outlook.com>
2023-08-07 21:53:26 -0300

    added new func and new options

--- caixa_eletrônico.py
+++ caixa_eletrônico.py
@@ -1,5 +1,6 @@
 # import sys
 # sys.path.insert(0, '/home/jx/Documents/hardware e software/git_projects/tools_py')
+from time import sleep
 from item_biggest_size import bsidict
 
 menu = '''
@@ -7,26 +8,31 @@ menu = '''
 			[d]epósito
 			[s]aque
 			[e]xtrato
-			[c]riar conta cliente
-			criar c[o]nta corrente
+			criar conta [u]ser
+			[c]riar conta corrente
+			
 			sai[r]
 -----------------------------------------------------------------
 '''
 # Variáveis default
-x = True
+# x = True
 deposito = []
 saque = []
 extrato = {'deposito': [], 'saque': []}
 saldo_total = 1000
 LIMITE_DIARIO = 3
+contas_users = [0,]
+contas_correntes = []
+agencia = '0001'
+num_conta = 1
+cpfs_cadastrados_contas = []
 
 # test output
-print(f'Deposito:{deposito}, Saque: {saque}, Extrato: {extrato}, Saldo Total: {saldo_total}, Limite Diário {LIMITE_DIARIO}\n')
+# print(f'Deposito:{deposito}, Saque: {saque}, Extrato: {extrato}, Saldo Total: {saldo_total}, Limite Diário {LIMITE_DIARIO}\n')
 
 
-def func_deposito():
+def func_deposito(quant_depo, /):
 	global deposito, extrato, saldo_total
-	quant_deposito = float(input('Quanto deseja depositar?: R$ '))
 
 	if quant_deposito < 0:
 		print("Valor de depósito é inválido! Por favor, ao tentar novamente insira um valor positivo!\n")
@@ -37,11 +43,13 @@ def func_deposito():
 		saldo_total += quant_deposito
 
 		# test output
-		print(f'Deposito:{deposito}, Saque: {saque}, Extrato: {extrato}, Saldo Total: {saldo_total}, Limite Diário {LIMITE_DIARIO}\n')
+		# print(f'Deposito:{deposito}, Saque: {saque}, Extrato: {extrato}, Saldo Total: {saldo_total}, Limite Diário {LIMITE_DIARIO}\n')
 
-def func_saque():
+
+
+
+def func_saque(*, quant_saq):
 	global saque, extrato, saldo_total, LIMITE_DIARIO
-	quant_saque = float(input('Quanto deseja saque?: R$ '))
 		
 	if quant_saque <= saldo_total and LIMITE_DIARIO > 0:
 		if quant_saque > 0:
@@ -51,7 +59,7 @@ def func_saque():
 			LIMITE_DIARIO -= 1
 			
 			# test output
-			print(f'\nDeposito:{deposito}, Saque: {saque}, Extrato: {extrato}, Saldo Total: {saldo_total}, Limite Diário {LIMITE_DIARIO}\n')
+			# print(f'\nDeposito:{deposito}, Saque: {saque}, Extrato: {extrato}, Saldo Total: {saldo_total}, Limite Diário {LIMITE_DIARIO}\n')
 
 	else:
 		print('\nValor de saque é inválido! Por favor, ao tentar novamente insira um valor positivo!')
@@ -63,7 +71,9 @@ def func_saque():
 		print("\nATENÇÃO! NÃO HÁ SALDO SUFICIENTE NA CONTA!")
 	
 
-def func_extrato():
+
+
+def func_extrato(sald='', /, *, extrat=''):
 	global extrato, saldo_total
 
 	if bsidict(extrato) == 0:
@@ -81,39 +91,94 @@ def func_extrato():
 		print('---------------------------------------------------------------------')
 
 		# test output
-		print(f'\nDeposito:{deposito}, Saque: {saque}, Extrato: {extrato}, Saldo Total: {saldo_total}, Limite Diário {LIMITE_DIARIO}\n')
+		# print(f'\nDeposito:{deposito}, Saque: {saque}, Extrato: {extrato}, Saldo Total: {saldo_total}, Limite Diário {LIMITE_DIARIO}\n')
+
+
+
+
+
+def criar_conta_user():
+	global contas_users
+
+	print('\n----Para criação de uma nova conta, por favor, insira os dados a seguir----')
+	nome = input('Nome: ')
+	data_nascimento = input('Data de nascimento: ')
+	while True:
+		n_cpf = input('Insira somente os números do seu CPF: ')
+		if len(n_cpf) == 11:
+			break
+		else:
+			print('___Atenção! Insira SOMENTE os números do seu CPF, nada além disso____')
+	
+	print('Insira seu endereço a seguir:')
+	endereco = {'Endereço':{'logradouro': input('Logradouro: '), 
+	     			'numero': input('Número: '), 
+	     			'bairro': input('Bairro: '), 'cidade': input('Cidade: '), 
+					'estado': input('Estado(Ex.: SP, RJ, ES.): ')}}
+	# endereco_id = f'Endereço: {endereco}'
+	
+	user = [nome, data_nascimento, int(n_cpf), endereco]
+	contas_users.append(user)
+	print(contas_users)
+	# criar_conta_corrente()
+
+
+
+
+
+def criar_conta_corrente():
+	global contas_users, contas_correntes, num_conta, agencia, cpfs_cadastrados_contas
+
+	print('\n----Para criação de uma nova conta corrente, por favor, insira os dados a seguir----')
+	cpf_id = int(input('Por favor insira seu CPF:'))
+	if cpf_id in cpfs_cadastrados_contas:
+		print('O seu CPF já tem cadastro em nosso sistema!')
+	else:
+		for user in contas_users:
+			# if contas_users[i[2]] == cpf_id:
+			if cpf_id == user[2]:
+				contas_correntes.append([agencia, num_conta, contas_users[num_conta]])
+				cpfs_cadastrados_contas.append(cpf_id)
+	
+	print(contas_correntes)
+	
+	print('...Criando conta corrente...')
+	print(contas_correntes)
+	print('\n---------------------conta criada com sucesso!---------------------')
+
 
 
 	
-
-def criar_conta_cliente():
-	...
-
-def criar_conta_corrente():
-	...
-
-def sair():
-	...
+	
 
 
-while x:
+
+
+
+
+
+
+
+while True:
 	question = input('para consultar o menu digite "m".')
 	if question == 'm':
 		print(menu)
 	
 	if question == 'd':
-		func_deposito()
+		quant_deposito = float(input('Quanto deseja depositar?: R$ '))
+		func_deposito(quant_deposito)
 
 	if question == 's':
-		func_saque()
+		quant_saque = float(input('Quanto deseja saque?: R$ '))
+		func_saque(quant_saq=quant_saque)
 	
 	if question == 'e':
 		func_extrato()
 
-	if question == 'c':
-		criar_conta_cliente()
+	if question == 'u':
+		criar_conta_user()
 	
-	if question == 'o':
+	if question == 'c':
 		criar_conta_corrente()
 					
 	elif question == 'i':
